a1b104f52dc4d41a2ad2efcc3d3558b62251f72d
sergiy-chumachenko <chumachenko.sergiy@gmail.com>
2018-04-28 13:42:51 +0300

    [UPDATE] Add Projects

--- budget/views.py
+++ budget/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Project
+from .models import Project, Category
 from django.views.generic import CreateView
+from django.http import HttpResponseRedirect
+from django.utils.text import slugify
+
 
 def project_list(request):
     return render(request, 'budget/project-list.html')
@@ -11,7 +14,24 @@ def project_detail(request, project_slug):
     expense_list = project.expenses.all()
     return render(request, 'budget/project-detail.html', {'project': project, 'expense_list': expense_list})
 
+
 class ProjectCreateView(CreateView):
     model = Project
     template_name = 'budget/add-project.html'
     fields = ('name', 'budget')
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.save()
+
+        categories = self.request.POST['categoriesString'].split(',')
+        for category in categories:
+            Category.objects.create(
+                project=Project.objects.get(id=self.object.id),
+                name=category
+            ).save()
+
+        return HttpResponseRedirect(self.get_success_url())
+
+    def get_success_url(self):
+        return slugify(self.request.POST['name'])
