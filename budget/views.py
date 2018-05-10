a48f9e0d6173d8872103286c39f577918fe5b889
sergiy-chumachenko <chumachenko.sergiy@gmail.com>
2018-05-10 22:20:40 +0300

    [UPDATE] Project List updated

--- budget/views.py
+++ budget/views.py
@@ -9,7 +9,8 @@ import json
 
 
 def project_list(request):
-    return render(request, 'budget/project-list.html')
+    project_list = Project.objects.all()
+    return render(request, 'budget/project-list.html', {'project_list': project_list})
 
 
 def project_detail(request, project_slug):
